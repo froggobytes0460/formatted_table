24a24ffecf3d3f8eaaf13374543ace88abb04950
Yasir Atiq Mohammed <yasiratiqmohammed@gmail.com>
2026-05-15 11:49:07 +0400

    Install `fmt` and `argparse` libraries to project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fmt/core.h>
+#include <argparse/argparse.hpp>
 
 
-int main() {
+int main(int argc, char** argv) {
+    // Use the ArgumentParser to parse command line arguments
+    argparse::ArgumentParser program("formatted-table", "0.1.0");
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+    
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
